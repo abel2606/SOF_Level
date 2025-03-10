236c133230f02233f627691ae715a2a9bfc9b431
abel2606 <abel.sanchez.guerrero@gmail.com>
2025-03-10 16:43:17 -0700

    Contenido

---
+++ SOF_Level/src/main/java/org/itson/sof_level/SOF_Level.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.sof_level;
+
+/**
+ *
+ * @author Abe
+ */
+public class SOF_Level {
+
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
